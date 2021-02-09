19ce73d1591b8bc2815b6aa22d7bd5707b06b9c7
SandWing <78792464+SandWing@users.noreply.github.com>
2021-02-09 18:13:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord, random
+import sll
+from discord.ext import commands
+from config import settings, TOKEN
+
+bot = commands.Bot(command_prefix='!')#префикс для команд
+
+@bot.command() 
+async def bot_on(ctx):
+    await ctx.send('Working-hard')
+
+@bot.command()
+async def help(text):
+    await text.send('Мои команды:\n1. !EGE *предмет* *номер задания* - выдает url ссылку на сайт решу ЕГЭ\n2. !bot_on - выводит статус бота, если он работает')
+
+@bot.command()
+async def hello(hi):
+    author = hi.message.author
+    await hi.send(f'go in peace, {author.mention}!')
+
+@bot.command()
+async def EGE(_, subject, exercise_number):
+    exercise_number=str(exercise_number)
+    url = 'https://'
+    url2 = 'ege.sdamgia.ru/'
+    ex_index = random.randint(0, (len(sll.all_tasks[subject][exercise_number])-1))
+    url_fin = url + sll.get_url[subject] + url2 + 'test?theme=' + sll.all_tasks[subject][exercise_number][ex_index]
+    await _.send("Here's your URL: {0}".format(url_fin))
+
+bot.run(TOKEN)
+
+'''TO-DO LIST:
+0. ввод будет тип: "!geturl *предмет* *номер задания*" COMPLETED
+1. прислать url к номерам ЕГЭ COMPLETED
+2. ввод для отдельного задания !getnumurl *предмет* *номер задания*
+'''
